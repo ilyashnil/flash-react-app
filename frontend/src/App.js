2504c2988da39acdc4615f77bf4bf5f86e58ff24
Ilya Shnayderman <ilyashn@il.ibm.com>
2022-05-10 12:22:49 +0300

    FROM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Hello World from React
+           Hello World FROM React
         </a>
       </header>
     </div>
