87ecf080fcad08b25a46551239fd462f7980b33d
Ilya Shnayderman <ilyashn@il.ibm.com>
2022-05-11 16:05:01 +0300

    submitting python code

---
+++ test/test_die.py
@@ -0,0 +1,9 @@
+from dice_package.dice import RollableDie
+
+
+class TestDie(unittest.TestCase):
+    def setUp(self):
+        self.die = RollableDie()
+
+    def test_upper(self):
+        self.assertEqual(self.die.sides, 6)
