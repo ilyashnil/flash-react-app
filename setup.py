3ab312056875302eb3694c511583196680738343
Ilya Shnayderman <ilyashn@il.ibm.com>
2022-05-23 16:34:32 +0300

    let's try again

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-#version = os.environ.get("RELEASE_VERSION", "latest1")
-version = os.environ["RELEASE_VERSION"]
 
 setuptools.setup(
     name="flaskreactapp",
-    version=version,
+    version="RELEASE_VERSION".replace('v',''),
     author="Ilya Shnayderman",
     author_email="ilyashn@il.ibm.com",
     description="Flask and react application",
