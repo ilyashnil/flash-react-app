a93152bfb2611196b5e5ade37b266d2469a0879d
Ilya Shnayderman <ilyashn@il.ibm.com>
2022-06-06 18:47:30 +0300

    first implementation of swagger

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,37 @@
 import os
 from flask import Flask, send_from_directory
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
+
+swagger_template = dict(
+    info={'title': 'My first Swagger UI document',
+          'version': '0.1',
+          'description': 'This document depicts a  Sleuth Swagger UI' },
+    )
+
+swagger_config = {"headers": [],
+                  "specs": [{
+                      "endpoint": 'hello-world',
+                      "route": '/hello_world',
+                      "rule_filter": lambda rule: True,
+                      "model_filter": lambda tag: True,
+                  }],
+                  "static_url_path": "/flasgger_static",
+                  "swagger_ui": True,
+                  "specs_route": "/apidocs/"}
+
 
 app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
+swagger = Swagger(app, template=swagger_template, config=swagger_config)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
+@swag_from("hello_world.yml", methods=['GET'])
+@app.route("/")
+def hello_world():
+    return "Hello World!!!"
 
 def import_by_reflection(name):
     components = name.split('.')
@@ -21,6 +46,7 @@ def hello():
     cls = import_by_reflection(class_name)
     obj = cls()
     return "Hello " + obj.get_suffix()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
